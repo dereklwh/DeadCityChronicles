2ab7ba481ec338a4a23c1fbaff4b83ef0e3edbd3
nkhaiduy <dna27@sfu.ca>
2023-11-01 22:38:01 -0700

    initialize tile size

--- phase2/src/main/java/com/group22/GamePanel.java
+++ phase2/src/main/java/com/group22/GamePanel.java
@@ -4,5 +4,7 @@ import javax.swing.JPanel;
 
 public class GamePanel extends JPanel{
 
-    
+    private final int originalTileSize = 16;
+    public int scale = 3;
+    public int tileSize = originalTileSize * scale;
 }
