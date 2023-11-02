e4af2defa5355344eae3cdad31026e4a2784dd64
Kai Nguyen <dna27@sfu.ca>
2023-11-01 22:56:34 -0700

    initialize screen size (can change later)

--- phase2/src/main/java/com/group22/GamePanel.java
+++ phase2/src/main/java/com/group22/GamePanel.java
@@ -7,4 +7,12 @@ public class GamePanel extends JPanel{
     private final int originalTileSize = 16;
     public int scale = 3;
     public int tileSize = originalTileSize * scale;
+
+    public final int maxScreenCol = 40;
+    public final int maxScreenRow = 22;
+
+    public int screenWidth = tileSize * maxScreenCol;
+    public int screenHeight = tileSize * maxScreenRow;
+
+    
 }
