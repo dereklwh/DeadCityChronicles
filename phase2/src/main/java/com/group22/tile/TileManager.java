af3a3b758a33f27aaf3296f20ba7a93ec399e3f1
Kai Nguyen <dna27@sfu.ca>
2023-11-09 21:16:38 -0800

    renamed Tile.java and removed unused libraries

--- phase2/src/main/java/com/group22/tile/TileManager.java
+++ phase2/src/main/java/com/group22/tile/TileManager.java
@@ -1,11 +1,9 @@
 package com.group22.tile;
 
-import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import java.awt.Graphics2D;
 import com.group22.GamePanel;
