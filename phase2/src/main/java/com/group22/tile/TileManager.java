886c2d4fcacce59e8c095b2ee65e015bc3cc745b
Kai Nguyen <dna27@sfu.ca>
2023-11-09 21:23:57 -0800

    changed tile spirte to player sprite for testing

--- phase2/src/main/java/com/group22/tile/TileManager.java
+++ phase2/src/main/java/com/group22/tile/TileManager.java
@@ -34,24 +34,24 @@ public class TileManager{
     public void getTileImage(){
         try{
             tile[0] = new Tile();
-            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass.png"));
+            tile[0].image = ImageIO.read(getClass().getResourceAsStream("../res/player/idle_0.png"));
 
             tile[1] = new Tile();
-            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/wall.png"));
-            tile[1].collision = true;
+            tile[1].image = ImageIO.read(getClass().getResourceAsStream("../res/player/idle_0.png"));
+            //tile[1].collision = true;
 
             tile[2] = new Tile();
-            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/bush.png"));
-            tile[2].collision = true; //add this line to make solid
+            tile[2].image = ImageIO.read(getClass().getResourceAsStream("../res/player/idle_0.png"));
+            //tile[2].collision = true; //add this line to make solid
 
             tile[3] = new Tile();
-            tile[3].image = ImageIO.read(getClass().getResourceAsStream("/tiles/xxx.png"));
+            tile[3].image = ImageIO.read(getClass().getResourceAsStream("../res/player/idle_0.png"));
 
             tile[4] = new Tile();
-            tile[4].image = ImageIO.read(getClass().getResourceAsStream("/tiles/yyy.png"));
+            tile[4].image = ImageIO.read(getClass().getResourceAsStream("../res/player/idle_0.png"));
 
             tile[5] = new Tile();
-            tile[5].image = ImageIO.read(getClass().getResourceAsStream("/tiles/zzz.png"));
+            tile[5].image = ImageIO.read(getClass().getResourceAsStream("../res/player/idle_0.png"));
 
         }catch(IOException e){
             e.printStackTrace();
