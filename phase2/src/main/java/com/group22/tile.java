a299f53a235455fc2afe2d4858b341a25edd7504
Kai Nguyen <dna27@sfu.ca>
2023-11-09 17:39:26 -0800

    move tile.java and TileManager.java inside tile folder

--- phase2/src/main/java/com/group22/tile.java
+++
@@ -1,9 +0,0 @@
-//under package tile
-
-package tile;
-
-public class Tile{
-
-    public BufferedImage image;
-    public boolean collision;
-}
--- phase2/src/main/java/com/group22/tile/TileManager.java
+++ phase2/src/main/java/com/group22/tile/TileManager.java
@@ -1,4 +1,6 @@
-package tile;
+package com.group22.tile;
+
+import com.group22.GamePanel;
 
 // under package Tile;
 
@@ -107,11 +109,11 @@ public class TileManager{
             int screenX = worldX - gp.player.worldX + gp.player.screenX;
             int screenY = worldY - gp.player.worldY + gp.player.screenY;
 
-            if (worldX > gp.player.worldX - gp.player.screenX &&
-                worldX < gp.player.worldX + gp.player.screenX &&
-                worldY > gp.player.worldY - gp.player.screenY &&
-                worldY < gp.player.worldY + gp.player.screenY){
-                    
+            if (worldX + gp.tileSize > gp.player.worldX - gp.player.screenX &&
+                worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+                worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
+                worldY - gp.tileSize < gp.player.worldY + gp.player.screenY){
+
                     g2.drawImage(tile[tileNum].image,screenX,screenY,gp.tileSize,gp.tileSize,null);
             }
             
