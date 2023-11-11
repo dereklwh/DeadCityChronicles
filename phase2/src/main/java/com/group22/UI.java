92fd9030a6f0fb869423cae1f490d4c26321a585
Kai Nguyen <dna27@sfu.ca>
2023-11-10 21:27:17 -0800

    added play time

--- phase2/src/main/java/com/group22/UI.java
+++ phase2/src/main/java/com/group22/UI.java
@@ -12,7 +12,7 @@ import com.group22.ObjectVaccine;
 public class UI {
     
     GamePanel gp;
-    Font arial_40; //Find new font later
+    Font arial_40, arial_80; //Find new font later
     BufferedImage keyImage;
     BufferedImage hImage;
     BufferedImage vImage;
@@ -20,11 +20,14 @@ public class UI {
     public boolean messageOn = false;
     public String message = "";
     int messageCounter = 0;
+    public boolean gameFinished = false;
 
+    double playTime;
 
     public UI(GamePanel gp) {
         this.gp = gp;
         arial_40 = new Font("Arial", Font.PLAIN, 40);
+        arial_80 = new Font("Arial", Font.BOLD, 80);
 
         ObjectKey key = new ObjectKey();
         keyImage = key.image;
@@ -43,26 +46,61 @@ public class UI {
 
     public void draw(Graphics2D g2) {
 
-        g2.setFont(arial_40);
-        g2.setColor(Color.white); 
+        if (gameFinished == true){
 
-        g2.drawImage(keyImage, gp.tileSize/2, gp.tileSize/2, gp.tileSize, gp.tileSize, null);
-        g2.drawString("x " + gp.player.hasKey, 74, 60);
+            g2.setFont(arial_40);
+            g2.setColor(Color.white); 
 
-        g2.drawImage(vImage, gp.tileSize/2 + 150, gp.tileSize/2, gp.tileSize, gp.tileSize, null);
-        g2.drawString("x " + gp.player.hasVaccine, 235, 60);
+            String text;
+            int textLength;
+            int x;
+            int y; 
 
-        if(messageOn == true){
+            text = "You won";
+            textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
 
-            g2.setFont(g2.getFont().deriveFont(30F));
-            g2.drawString(message, gp.tileSize/2, gp.tileSize*5);
+            x = gp.screenWidth/2 - textLength/2;
+            y= gp.screenHeight/2 - (gp.tileSize * 3);
+            g2.drawString(text, x, y);
 
-            messageCounter++;
+            g2.setFont(arial_80);
+            g2.setColor(Color.yellow);
+            text = "Congratulations";
+            textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
 
-            if(messageCounter > 120){
-                messageCounter =0;
-                messageOn = false;
+            x = gp.screenWidth/2 - textLength/2;
+            y= gp.screenHeight/2 + (gp.tileSize * 2);
+            g2.drawString(text, x, y);
+
+            gp.gameThread = null;
+        }
+
+        else {
+            g2.setFont(arial_40);
+            g2.setColor(Color.white); 
+
+            g2.drawImage(keyImage, gp.tileSize/2, gp.tileSize/2, gp.tileSize, gp.tileSize, null);
+            g2.drawString("x " + gp.player.hasKey, 74, 60);
+
+            g2.drawImage(vImage, gp.tileSize/2 + 150, gp.tileSize/2, gp.tileSize, gp.tileSize, null);
+            g2.drawString("x " + gp.player.hasVaccine, 235, 60);
+            
+            playTime += (double)1/60;
+            g2.drawString("Time:" + playTime, gp.tileSize*11, 60);
+            
+            if(messageOn == true){
+
+                g2.setFont(g2.getFont().deriveFont(30F));
+                g2.drawString(message, gp.tileSize/2, gp.tileSize*5);
+
+                messageCounter++;
+
+                if(messageCounter > 120){
+                    messageCounter =0;
+                    messageOn = false;
+                }
             }
         }
+        
     }
 }
