9009ba0b4fe0f556ed40cce68b0fc1be63fd5d6a
Kai Nguyen <dna27@sfu.ca>
2023-11-10 19:35:05 -0800

    Set UI font

--- phase2/src/main/java/com/group22/UI.java
+++ phase2/src/main/java/com/group22/UI.java
@@ -1,5 +1,18 @@
 package com.group22;
 
+import java.awt.Graphics2D;
+import java.awt.Font;
+
 public class UI {
     
+    GamePanel gp;
+
+    public UI(GamePanel gp) {
+        this.gp = gp;
+    }
+
+    public void draw(Graphics2D g2) {
+
+        g2.setFont(new Font("Arial", Font.PLAIN, 40)); //Find new font later
+    }
 }
