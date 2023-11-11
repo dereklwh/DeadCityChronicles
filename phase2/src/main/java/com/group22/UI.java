a32e5f2726ed14ce42c18d82d0df35ec435b34e4
Kai Nguyen <dna27@sfu.ca>
2023-11-10 19:48:48 -0800

    Created objects in UI class

--- phase2/src/main/java/com/group22/UI.java
+++ phase2/src/main/java/com/group22/UI.java
@@ -1,23 +1,40 @@
 package com.group22;
 
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.awt.Color;
 import java.awt.Font;
+
+import com.group22.ObjectKey;
+import com.group22.ObjectHeart;
+import com.group22.ObjectVaccine;
 
 public class UI {
     
     GamePanel gp;
     Font arial_40; //Find new font later
+    BufferedImage keyImage;
+    BufferedImage hImage;
+    BufferedImage vImage;
 
     public UI(GamePanel gp) {
         this.gp = gp;
         arial_40 = new Font("Arial", Font.PLAIN, 40);
+
+        ObjectKey key = new ObjectKey();
+        keyImage = key.image;
+
+        ObjectHeart heart = new ObjectHeart();
+        hImage = heart.image;
+
+        ObjectVaccine vaccine = new ObjectVaccine();
+        vImage = vaccine.image;
     }
 
     public void draw(Graphics2D g2) {
 
         g2.setFont(arial_40);
         g2.setColor(Color.white); 
-        g2.drawString("Key = " + gp.player.hasKey, 50, 50);
+        g2.drawString("Key = " + gp.player.hasKey, 25, 50);
     }
 }
