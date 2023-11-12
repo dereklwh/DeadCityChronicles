f8dbc0dce0ad76a29f7bbcd8ee14bcf0f89bbf6a
Kai Nguyen <dna27@sfu.ca>
2023-11-12 02:32:44 -0800

    created objectDoor

---
+++ phase2/src/main/java/com/group22/ObjectDoor.java
@@ -0,0 +1,19 @@
+package com.group22;
+
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class ObjectDoor extends SuperObject {
+    GamePanel gp;
+    public ObjectDoor(GamePanel gp){
+        name = "Door";
+        try{
+            image = ImageIO.read(getClass().getResourceAsStream("res/object/key.png"));
+            image = uTool.scaleImage(image, gp.tileSize, gp.tileSize);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        collision = true;
+    }
+}
