4e789fb5fc253b88d048beaa27f95186d9e501ef
Kai Nguyen <dna27@sfu.ca>
2023-11-11 22:07:34 -0800

    placed more objects

--- phase2/src/main/java/com/group22/AssetSetter.java
+++ phase2/src/main/java/com/group22/AssetSetter.java
@@ -14,7 +14,11 @@ public class AssetSetter {
 
         gp.obj[1] = new ObjectVaccine(gp);
         gp.obj[1].worldX = 23 * gp.tileSize; //change this to anywhere we want to put the object
-        gp.obj[1].worldY = 2 * gp.tileSize; //change this as well
+        gp.obj[1].worldY = 33 * gp.tileSize; //change this as well
+
+         gp.obj[2] = new ObjectVaccine(gp);
+        gp.obj[2].worldX = 12 * gp.tileSize; 
+        gp.obj[2].worldY = 25 * gp.tileSize;
     }
 
     public void setZombie() {
