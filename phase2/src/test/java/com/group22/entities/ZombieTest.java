8ecbef216e4b7676e7db88d78984d6592735b866
Derek <derek141772@gmail.com>
2023-11-29 00:13:15 -0800

    added test cases + commented out failing test case

--- phase2/src/test/java/com/group22/entities/ZombieTest.java
+++ phase2/src/test/java/com/group22/entities/ZombieTest.java
@@ -3,6 +3,8 @@ package com.group22.entities;
 import com.group22.GamePanel;
 import com.group22.KeyHandler;
 
+import static org.mockito.Mockito.when;
+
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import junit.framework.TestCase;
@@ -50,4 +52,35 @@ public class ZombieTest extends TestCase {
         String expectedDirection = "right";
         assertEquals(expectedDirection, zombie.direction);
     }
+
+    public void testSpriteAnimationUpdate() {
+        // Call update method 50 times
+        for (int i = 0; i < 50; i++) {
+            zombie.update();
+        }
+        // Assert that spriteNum cycles through 1 to 4
+        assertTrue("Sprite number should be between 1 and 4", zombie.spriteNum >= 1 && zombie.spriteNum <= 4);
+    }
+
+    public void testZombieRemovalAfterCollisionWithVaccinatedPlayer() {
+        // Set up a scenario where the player has a vaccine
+        mockPlayer.hasVaccine = 1;
+    
+        // Position the zombie to collide with the player
+        zombie.worldX = mockPlayer.worldX;
+        zombie.worldY = mockPlayer.worldY;
+    
+        // Mock collision checker for player collision
+        when(mockGamePanel.cChecker.checkPlayer(zombie, zombie.worldX, zombie.worldY)).thenReturn(true);
+    
+        // Call update
+        zombie.update();
+    
+        // Assert that the zombie is marked for removal
+        assertTrue("Zombie should be marked for removal after colliding with a vaccinated player", zombie.isRemoveThis());
+    }
+    
+
+
+
 }
