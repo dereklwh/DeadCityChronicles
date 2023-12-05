ee6a83ddcc2e5c8fdd962d08733bd9fbd36fef9b
Sina <sma231@sfu.ca>
2023-12-04 16:49:26 -0800

    removed checkPlayer method

--- phase2/src/main/java/com/group22/CollisionChecker.java
+++ phase2/src/main/java/com/group22/CollisionChecker.java
@@ -232,46 +232,6 @@ public class CollisionChecker {
         return index;
     }
 
-    // public void checkPlayer(Entity entity){
-    //             entity.solidArea.x = entity.worldX + entity.solidArea.x;
-    //             entity.solidArea.y = entity.worldY + entity.solidArea.y;
-    //             //Get obj's solid area pos
-    //             gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
-    //             gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
-
-    //             switch (entity.direction) {
-    //                 case "up": //print for test remove later
-    //                     entity.solidArea.y -= entity.speed;
-    //                     if (entity.solidArea.intersects(gp.player.solidArea)) {
-    //                             entity.collisionOn = true;
-    //                         }
-    //                     break;
-    //                 case "down":
-    //                     entity.solidArea.y += entity.speed;
-    //                     if (entity.solidArea.intersects(gp.player.solidArea)) {
-    //                             entity.collisionOn = true;
-    //                     }
-    //                     break;
-    //                 case "left":
-    //                     entity.solidArea.x -= entity.speed;
-    //                     if (entity.solidArea.intersects(gp.player.solidArea)) {
-    //                             entity.collisionOn = true;
-    //                     }
-    //                     break;
-    //                 case "right":
-    //                     entity.solidArea.x += entity.speed;
-    //                     if (entity.solidArea.intersects(gp.player.solidArea)) {
-    //                             entity.collisionOn = true;
-    //                         }
-    //                         //System.out.println("right collision");
-    //                         break;
-    //             }
-    //             entity.solidArea.x = entity.solidAreaDefaultX;
-    //             entity.solidArea.y = entity.solidAreaDefaultY;
-    //             gp.player.solidArea.x = gp.player.solidAreaDefaultX;
-    //             gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-    // }
-
     public boolean checkPlayer(Entity entity, int nextX, int nextY) {
         // Calculate the entity's next solid area position
         Rectangle nextSolidArea = new Rectangle(nextX + entity.solidArea.x,
