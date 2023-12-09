6178aeb340be0917064f8ca5d3d26703394add44
Kai Nguyen <dna27@sfu.ca>
2023-12-08 22:07:10 -0800

    created jacoco test result

--- phase2/target/site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.group22"},{"l":"com.group22.entities"},{"l":"com.group22.objects"},{"l":"com.group22.tile"}];updateSearchResults();
